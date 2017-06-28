49b6820eacff5686c1557eee452e6d50920ed165
George Berezhnoy <gohabereg@users.noreply.github.com>
2017-06-28 20:58:49 +0300

    Comments

--- src/notifies.js
+++ src/notifies.js
@@ -29,6 +29,25 @@ module.exports = (function () {
 
   }
 
+  /**
+   * Show new notification
+   *
+   * @param {Object} options:
+   *
+   * @property {String} type    - type of notification. Alert, confirm or prompt (alert by default)
+   * @property {String} message - text to notify, can contains HTML
+   * @property {String} style   - style of notification. Just add 'cdx-notify--' + type class. ('success' or 'error' for example)
+   * @property {Number} time    - (optional) expiring time (8 sec by default)
+   *
+   * Optional properties for confirm and prompt types
+   * @property {String} okText          - text for confirmation button
+   * @property {String} cancelText      - (for confirm type only) text for cancel button
+   * @property {Function} okHandler     - confirmation (or user's input) handler
+   * @property {Function} cancelHandler - cancel handler (when cancel button was pressed or notification was closed)
+   * @property {String} placeholder     - (for prompt type only) input placeholder
+   * @property {String} default         - (for prompt type only) input initial value
+   * @property {String} inputType       - (for prompt type only) prompt input type
+   */
   function show(options) {
 
     if (!options.message) {
