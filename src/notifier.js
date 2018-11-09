5fed4731ef8747d161970339fd1bc6f8051424e3
Taly <vitalik7tv@yandex.ru>
2018-11-09 12:42:20 +0300

    Add interfaces file

--- src/notifier.js
+++ src/notifier.js
@@ -14,9 +14,8 @@ module.exports = (function () {
   let wrapper_ = null;
 
   /**
-   *
-   * @return {boolean}
    * @private
+   * @return {boolean}
    */
   function prepare_() {
     if (wrapper_) {
@@ -31,21 +30,7 @@ module.exports = (function () {
   /**
    * Show new notification
    *
-   * @param {Object} options:
-   *
-   * @property {String} type    - type of notification. Alert, confirm or prompt (alert by default)
-   * @property {String} message - text to notify, can contains HTML
-   * @property {String} style   - style of notification. Just add 'cdx-notify--' + type class. ('success' or 'error' for example)
-   * @property {Number} [time=8000]  - expiring time (8 sec by default)
-   *
-   * Optional properties for confirm and prompt types
-   * @property {String} [okText]          - text for confirmation button
-   * @property {Function} [okHandler]     - confirmation (or user's input) handler
-   * @property {String} [cancelText]      - (for confirm type only) text for cancel button
-   * @property {Function} [cancelHandler] - cancel handler (when cancel button was pressed or notification was closed)
-   * @property {String} [placeholder]     - (for prompt type only) input placeholder
-   * @property {String} [default]         - (for prompt type only) input initial value
-   * @property {String} [inputType]       - (for prompt type only) prompt input type
+   * @param {NotificationOptions | ConfirmNotificationOptions  | PromptNotificationOptions} options
    */
   function show(options) {
     if (!options.message) {
@@ -55,6 +40,7 @@ module.exports = (function () {
     prepare_();
 
     let notify = null;
+
     const time = options.time || 8000;
 
     switch (options.type) {
